fdcccb507f814b96b947795d7416d89a165f44b2
Victor Marques Ribeiro <victormarquess11@gmail.com>
2022-09-19 01:51:00 -0300

    Changed Repeat function to use standard library strings.Repeat

--- repeat.go
+++ repeat.go
@@ -1,8 +1,11 @@
 package iteration
 
+import "strings"
+
 func Repeat(input string, repeatCount int) (output string) {
-	for i := 0; i < repeatCount; i++ {
-		output += input
-	}
-	return output
+	//	for i := 0; i < repeatCount; i++ {
+	//		output += input
+	//	}
+	//	return output
+	return strings.Repeat(input, repeatCount)
 }
